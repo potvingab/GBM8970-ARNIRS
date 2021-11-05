33b802558af65290b647aecf32be1c705c0a5ab6
khadijayakoubi <khadijayakoubi5245@gmail.com>
2021-11-04 21:36:21 -0400

    affichage question

---
+++ VRNIRS/Assets/C# Scripts/#C script Khadija/RED.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+
+public class RED : MonoBehaviour {
+
+    public Button redButton;
+	void Start () {
+        Button btnState = redButton.GetComponent<Button>();
+        btnState.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the red button!");
+        Questions.randQuestion = -1;
+        Questions.selectedAnswers = "RED";
+        string createText = "RED" + Environment.NewLine;
+        File.AppendAllText(@"C:\Users\HP\Documents\4 année\Projet Intégrateur\answers.txt", createText);
+
+   
+    }
+}

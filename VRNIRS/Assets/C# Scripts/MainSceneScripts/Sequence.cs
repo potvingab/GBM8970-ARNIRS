98b9c791477a2ac47490c4068bdb43aa79802484
Gabriel Potvin <gabriel.potvin@polymtl.ca>
2021-11-13 18:39:27 -0500

    Transférer variables entre scènes, dropdown menu pour choix du paradigme

--- VRNIRS/Assets/C# Scripts/MainSceneScripts/Sequence.cs
+++ VRNIRS/Assets/C# Scripts/MainSceneScripts/Sequence.cs
@@ -13,6 +13,11 @@ public class Sequence : MonoBehaviour {
 	public GameObject Dropdown4;
 	public GameObject Dropdown5;
 	public GameObject Dropdown6;
+	public GameObject DropdownLevel2;
+	public GameObject DropdownLevel3;
+	public GameObject DropdownLevel4;
+	public GameObject DropdownLevel5;
+	public GameObject DropdownLevel6;
 
 	public void StoreNumber() {
 		string nb = inputField.GetComponent<Text>().text;
@@ -24,6 +29,11 @@ public class Sequence : MonoBehaviour {
 			Dropdown4.SetActive(false);
 			Dropdown5.SetActive(false);
 			Dropdown6.SetActive(false);
+			DropdownLevel2.SetActive(false);
+			DropdownLevel3.SetActive(false);
+			DropdownLevel4.SetActive(false);
+			DropdownLevel5.SetActive(false);
+			DropdownLevel6.SetActive(false);
 		}
 		if (numberTrials == 2){
 			Dropdown2.SetActive(true);
@@ -31,6 +41,11 @@ public class Sequence : MonoBehaviour {
 			Dropdown4.SetActive(false);
 			Dropdown5.SetActive(false);
 			Dropdown6.SetActive(false);
+			DropdownLevel2.SetActive(true);
+			DropdownLevel3.SetActive(false);
+			DropdownLevel4.SetActive(false);
+			DropdownLevel5.SetActive(false);
+			DropdownLevel6.SetActive(false);
 		}
 		if (numberTrials == 3){
 			Dropdown2.SetActive(true);
@@ -38,6 +53,11 @@ public class Sequence : MonoBehaviour {
 			Dropdown4.SetActive(false);
 			Dropdown5.SetActive(false);
 			Dropdown6.SetActive(false);
+			DropdownLevel2.SetActive(true);
+			DropdownLevel3.SetActive(true);
+			DropdownLevel4.SetActive(false);
+			DropdownLevel5.SetActive(false);
+			DropdownLevel6.SetActive(false);
 		}
 		if (numberTrials == 4){
 			Dropdown2.SetActive(true);
@@ -45,6 +65,11 @@ public class Sequence : MonoBehaviour {
 			Dropdown4.SetActive(true);
 			Dropdown5.SetActive(false);
 			Dropdown6.SetActive(false);
+			DropdownLevel2.SetActive(true);
+			DropdownLevel3.SetActive(true);
+			DropdownLevel4.SetActive(true);
+			DropdownLevel5.SetActive(false);
+			DropdownLevel6.SetActive(false);
 		}
 		if (numberTrials == 5){
 			Dropdown2.SetActive(true);
@@ -52,13 +77,24 @@ public class Sequence : MonoBehaviour {
 			Dropdown4.SetActive(true);
 			Dropdown5.SetActive(true);
 			Dropdown6.SetActive(false);
+			DropdownLevel2.SetActive(true);
+			DropdownLevel3.SetActive(true);
+			DropdownLevel4.SetActive(true);
+			DropdownLevel5.SetActive(true);
+			DropdownLevel6.SetActive(false);
 		}
 		if (numberTrials >= 6){
+			numberTrials = 6;
 			Dropdown2.SetActive(true);
 			Dropdown3.SetActive(true);
 			Dropdown4.SetActive(true);
 			Dropdown5.SetActive(true);
 			Dropdown6.SetActive(true);
+			DropdownLevel2.SetActive(true);
+			DropdownLevel3.SetActive(true);
+			DropdownLevel4.SetActive(true);
+			DropdownLevel5.SetActive(true);
+			DropdownLevel6.SetActive(true);
 		}
 	}
 	
---
+++ VRNIRS/Assets/C# Scripts/SampleSceneSciprts/ChangeText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour {
+	public GameObject Text;
+
+	public void Update () {
+		Text.GetComponent<Text>().text = VariablesHolderStroop.stroopTrialTime.ToString() + " secs, " + VariablesHolderStroop.stroopNumberTrials.ToString() + " trials \nSequence: " + String.Join(", ", VariablesHolderStroop.stroopSequence.ToArray()) + "\nLevels: " + String.Join(", ", VariablesHolderStroop.stroopSequenceLevels.Select(x => x.ToString()).ToArray()) + "\nMode: " + VariablesHolderStroop.stroopGameMode;
+	}
+}

9b5a958ddd688c22b054072b65b73e9d02f14231
Justineloignonlapointe <58313665+Justineloignonlapointe@users.noreply.github.com>
2021-11-16 13:33:54 -0500

    test to use World canvas and test for handtracking

---
+++ VRNIRS/Assets/C# Scripts/Response.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta;
+using Meta.HandInput;
+using UnityEngine.SceneManagement;
+
+
+
+public class Response : Interaction
+{
+
+    public GameObject _boutton;
+    
+
+
+    private HandFeature _handFeature; //follow the hand during a grab
+    private GameObject _selectedGameObject;
+      
+    protected override void Engage() //when it's in the zone, hand close
+    {
+        _handFeature = GrabbingHands[0];
+
+        if (_handFeature == null)
+
+        {
+            _selectedGameObject = Instantiate(_boutton); //create a clone to move  object
+            
+        }
+
+        Debug.Log("grabbed");
+        //appeler la fonction du on clic
+        
+
+
+    }
+    protected override void Disengage()
+    {
+        if (_handFeature == null || _selectedGameObject == null)
+
+        {
+
+            return;
+        }
+
+        _selectedGameObject.SendMessage("detach");
+        _selectedGameObject = null;
+        
+        
+        
+
+    }
+
+    protected override void Manipulate()
+    {
+        return;
+    }
+}
